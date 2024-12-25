a045c82263f6a0a98aef0497aea221bef872787a
561sharath <sharathsardar1432@gmail.com>
2024-12-25 12:24:31 +0530

    logout api, edit api

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,9 +1,10 @@
 
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
+const { validateProfileEditData } = require("../utils/validation");
 const profileRouter = express.Router();
 
-profileRouter.get('/profile', userAuth, async (req, res) => {
+profileRouter.get('/profile/view', userAuth, async (req, res) => {
 
     try {
   
@@ -23,6 +24,32 @@ profileRouter.get('/profile', userAuth, async (req, res) => {
   
   
   
+  })
+
+  profileRouter.patch('/profile/edit', userAuth, async (req,res) => {
+
+    try{
+        if (!validateProfileEditData(req)){
+            throw new Error("Invalid edit fields")
+        }
+
+        const loggedInUser = req.user
+
+        Object.keys(req.body).forEach((field) => {
+            loggedInUser[field] = req.body[field]
+        })
+
+        console.log(loggedInUser)
+
+        await loggedInUser.save()
+
+        res.json({message: "Profile edited successfully", user: loggedInUser})
+        
+    }catch(err){
+        console.error(err)
+        res.status(400).json({ message: "Error editing profile", error: err.message })
+    }
+   
   })
 
   module.exports = profileRouter;
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,24 +1,34 @@
 
 const validator = require('validator')
+const { all } = require('../routes/auth')
 const validateSignUpData = (req) => {
 
-    const {firstName, lastName, emailId, password} = req.body
+    const { firstName, lastName, emailId, password } = req.body
 
-    if(!firstName || !lastName) {
+    if (!firstName || !lastName) {
         throw new Error(" Name fields are mandatory")
     }
-    else if (firstName.length < 4 || firstName.length > 50){
+    else if (firstName.length < 4 || firstName.length > 50) {
         throw new Error("First Name should be between 4 and 50 characters")
     }
 
-    else if(!validator.isEmail(emailId)){
+    else if (!validator.isEmail(emailId)) {
         throw new Error("Invalid Email")
     }
 
-    else if (!validator.isStrongPassword(password)){
+    else if (!validator.isStrongPassword(password)) {
         throw new Error("Password should be atleast 8 characters long and should contain atleast one lowercase, one uppercase, one numeric and one special character")
     }
 
 }
 
-module.exports = {validateSignUpData}
+const validateProfileEditData = (req) => {
+
+    const allowedEditFileds = ['firstName', 'lastName', 'emailId', 'skills', 'about', 'photoUrl']
+
+    const isEditAllowed = Object.keys(req.body).every((filed) => allowedEditFileds.includes(filed))
+
+    return isEditAllowed
+}
+
+module.exports = { validateSignUpData, validateProfileEditData }
